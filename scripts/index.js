81b32644bc5d443604234702c60dd144244326fc
Ahmed <ahmed.max7586@gmail.com>
2019-07-17 17:04:28 -0400

    added display fn

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,23 @@ function handleSubmitNew(){
 		const tipPercentage = $('#tip-percentage').val();
 		const result = utils.calculateCharges(baseMeal, taxRate, tipPercentage);
 		console.log(result);
+		store.setCharges(result['subtotal'], result['tip']);
+		dispplayInfo();
+
 	});
 }
+
+function dispplayInfo () {
+
+$('.subtotal').html(store.subtotal);
+$('.tip').html(store.tip);
+$('.total').html(store.total);
+$('.tipTotal').html(store.tipTotal);
+$('.mealCount').html(store.mealCount);
+$('.averageTipMeal').html(store.averageTipMeal);
+
+}
+
 
 
 function bindEventListeners(){
